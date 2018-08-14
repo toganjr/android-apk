e6af4daa9fc1bf4880227cb3d77bcbb3ade06a89
toganjr <Yugijr@gmail.com>
2018-08-14 14:16:31 +0700

    fix issue Drawer

--- app/src/main/java/com/example/user/navbartemplatejava/MenuActivity.java
+++ app/src/main/java/com/example/user/navbartemplatejava/MenuActivity.java
@@ -7,6 +7,7 @@ import android.preference.PreferenceActivity;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -37,36 +38,39 @@ public class MenuActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mPrefs = ((InkaApp)  getApplication()).getPrefs();
+        if (menufrom == null){
+            setContentView(R.layout.activity_main);
+        } else if (menufrom == "registration"){
+            setContentView(R.layout.activity_display_ncr_registration);
+        } else if (menufrom == "profile") {
+            setContentView(R.layout.activity_prof);
+        } else if(menufrom == "verification"){
+            setContentView(R.layout.activity_verifikasi);
+        }
 
+        mPrefs = ((InkaApp)  getApplication()).getPrefs();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         View header = navigationView.getHeaderView(0);
-
-            if (menufrom == null){
-                setContentView(R.layout.activity_main);
-                mNama = header.findViewById(R.id.nama);
-                mNip = header.findViewById(R.id.nipHeader);
-
-                mNip.setText(mPrefs.getUserSignIn().getNip());
-                mNama.setText(mPrefs.getUserSignIn().getName());
-
-
-            } else if (menufrom == "registration"){
-                setContentView(R.layout.activity_display_ncr_registration);
-            } else if (menufrom == "profile") {
-                setContentView(R.layout.activity_prof);
-            } else if(menufrom == "verification"){
-                setContentView(R.layout.activity_verifikasi);
-            }
         navigationView.setNavigationItemSelectedListener(this);
 
+        mNama = header.findViewById(R.id.nama);
+        mNip = header.findViewById(R.id.nipHeader);
+
+        mNama.setText(mPrefs.getUserSignIn().getName());
+        mNip.setText(mPrefs.getUserSignIn().getNip());
+
+
     }
+
+
 
 
     @Override
